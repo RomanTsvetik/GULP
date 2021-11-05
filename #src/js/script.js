8bc5ddf75c26b37f49863635a312d259cb3a0015
RomanTsvetik <zvetik85@gmail.com>
2021-11-05 17:06:17 +0200

    gulp first commit

---
+++ #src/js/script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+@@include('alert.js')
+
+// function testWebP(callback) {
+//     var webP = new Image();
+//     webP.onload = webP.onerror = function () {
+//         callback(webP.height == 2);
+//     };
+//     webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+// };
+
+// testWebP(function(support) {
+//       if(support) {
+//           //Do what you whant =)
+//           document.querySelector('body').classList.add('webp')
+//          console.log('work webp');
+//       }else{
+//           //Do what you whant =)
+//          console.log('not work, use jgp/png')
+//       }
+      
+// })
